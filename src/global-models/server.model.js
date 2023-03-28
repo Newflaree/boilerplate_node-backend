43dec5898035834d52cdd5db5df252aa3d0c7c46
Camilo López <camiloj.lopez79@gmail.com>
2023-03-28 13:49:12 -0300

    [CONFIG]: Created initial config for Server model

--- src/global-models/server.model.js
+++ src/global-models/server.model.js
@@ -1,10 +1,37 @@
+// Express 
+import express from 'express';
+// Cors
+import cors from 'cors';
+
 class Server {
   constructor() {
+    this.app = express();
     this.port = process.env.PORT || '3001';
+
+    // DB Connection
+    // Initial methods
+    this.middlewares();
+  }
+
+  middlewares() {
+    this.app.use( cors() );
+    this.app.use( express.json() );
+  }
+
+  routes() {
+    throw new Error( 'Method not implemented yet' )
   }
 
   listen() {
-    console.log( `Listening on port ${ this.port.green }` );
+    this.app.listen( this.port, () => {
+      console.clear();
+
+      try {
+        console.log( `${ '[SERVER.LISTEN]'.bgGreen }: Server listening on port ${ this.port.green }` );
+      } catch ( err ) {
+        console.log( `${ '[SERVER.LISTEN]'.bgRed }: ${ err }` );
+      }
+    });
   }
 }
 
